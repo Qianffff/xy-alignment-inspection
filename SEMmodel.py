05d95251c3fe97fd53530793ab4921ee6e47e8d4
JartKoster <jart.koster@outlook.com>
2025-09-12 09:45:49 +0200

    -

--- SEMmodel.py
+++ SEMmodel.py
@@ -478,6 +478,6 @@ if __name__ == "__main__":
     black_white_grid = detect_and_plot_harris_corners(picture_grid_denoised,dot_radius=1,dot_alpha=0.25,k=0.24)
    
     # Finding the rotation using the high contrast plot
-    found_rotation = find_rotation(black_white_grid,0,0,cross_length=cross_length,cross_width=cross_line_width,frame_width_x=frame_width_x,frame_width_y=frame_width_y)
+    found_rotation = find_rotation(black_white_grid,shift_x,shift_y,cross_length=cross_length,cross_width=cross_line_width,frame_width_x=frame_width_x,frame_width_y=frame_width_y)
     print(f"Found rotation = {found_rotation}")
     print(f"Angle error = {found_rotation-rotation:.2f}")

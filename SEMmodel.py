16e423940d86eab6bbee4621d26168908bc0e94b
Olivier-Vos <oliviervos@protonmail.com>
2025-09-19 16:09:23 +0200

    -

--- SEMmodel.py
+++ SEMmodel.py
@@ -6,13 +6,13 @@ from Function_graveyard import *
 from Variables_and_constants import *
 
 # Generate wafer image
-grid, pixel_width_real_x, pixel_width_real_y, pixels_real_x, pixels_real_y, shift_real_x, shift_real_y, rotation = real_image(pixel_width_real_x,pixel_width_real_y,frame_width_x,frame_width_y,cross_length,cross_line_width)  
+grid, pixel_width_real, pixels_real_x, pixels_real_y, shift_real_x, shift_real_y, rotation = real_image(pixel_width_real,frame_width_x,frame_width_y,cross_length,cross_line_width)  
 
 # Resize image to go from real/original pixel width to measure pixel width
-grid_resampled = resample_image_by_pixel_size(grid, pixel_width_real_x, pixel_width_x)
+grid_resampled = resample_image_by_pixel_size(grid, pixel_width_real, pixel_width)
 
 # Use Gaussian distribution to meassure image
-picture_grid, half_pixel_width_gaussian_kernel, sigma = measured_image(grid_resampled, pixel_width_x, pixel_width_y, beam_current, scan_time_per_pixel)
+picture_grid, half_pixel_width_gaussian_kernel, sigma = measured_image(grid_resampled, pixel_width, beam_current, scan_time_per_pixel)
 
 # Denoise the image
 picture_grid_denoised = denoise_image(picture_grid)
@@ -20,7 +20,7 @@ picture_grid_denoised = denoise_image(picture_grid)
 # Position of the cross
 centerx, centery, cross_points = cross_position(picture_grid_denoised,intensity_threshold)
 # Difference between calculated cross center position and actual position (in m)
-absolute_distance_error = np.linalg.norm([int(np.round(pixels_real_x/2+shift_real_x)) - centerx*resize_factor,int(np.round(pixels_real_y/2+shift_real_y)) - centery*resize_factor])*pixel_width_real_x
+absolute_distance_error = np.linalg.norm([int(np.round(pixels_real_x/2+shift_real_x)) - centerx*resize_factor,int(np.round(pixels_real_y/2+shift_real_y)) - centery*resize_factor])*pixel_width_real
 
 # Listing some values of variables used in the simulation
 time_to_make_picture = (pixels_real_x/resize_factor)*(pixels_real_y/resize_factor)*scan_time_per_pixel

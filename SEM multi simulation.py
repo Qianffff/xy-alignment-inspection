48708a1af95dc20a238b5b67d0d36f8aefed2d95
Olivier-Vos <oliviervos@protonmail.com>
2025-09-20 02:27:20 +0200

    -

--- SEM multi simulation.py
+++ SEM multi simulation.py
@@ -22,14 +22,14 @@ def simulate(procedure):
         frame_width = procedure[step][0] # Get the frame_width for the current step
         # Narrow the scanning area (except when it's the first step)
         if step != 0:
-            grid = grid[int(np.round(max(0,cross_center_measured_px[0]-frame_width/pixel_width_real/2))):
-                        int(np.round(min(pixels_real,cross_center_measured_px[0]+frame_width/pixel_width_real/2))),
-                        int(np.round(max(0,int(cross_center_measured_px[1]-frame_width/pixel_width_real/2)))):
-                        int(np.round(min(pixels_real,int(cross_center_measured_px[1]+frame_width/pixel_width_real/2))))]
-            
+            zoomed_grid = grid[int(np.round(max(0,cross_center_measured_px[0]-frame_width/pixel_width_real/2))):
+                          int(np.round(min(pixels_real,cross_center_measured_px[0]+frame_width/pixel_width_real/2))),
+                          int(np.round(max(0,cross_center_measured_px[1]-frame_width/pixel_width_real/2))):
+                          int(np.round(min(pixels_real,cross_center_measured_px[1]+frame_width/pixel_width_real/2)))]
+        else: zoomed_grid = grid
         pixel_width, SNR = procedure[step][1], procedure[step][2] # Get the pixel_width and SNR for the current step
         # Resample grid to go from the real pixel width to the image pixel width
-        grid_resampled = resample_image_by_pixel_size(grid, pixel_width)
+        grid_resampled = resample_image_by_pixel_size(zoomed_grid, pixel_width)
         # Simulate the scanning of the image with the e-beam
         picture_grid = measured_image(grid_resampled,pixel_width,SNR)
         # Denoise the image
@@ -37,12 +37,13 @@ def simulate(procedure):
         
         # Calculate the position of the center of the cross
         cross_center_measured_px = cross_position(picture_grid_denoised,intensity_threshold)
-        cross_center_measured =  cross_center_measured_px * pixel_width # Convert from pixels to meters
+        cross_center_measured = cross_center_measured_px * pixel_width # Convert from pixels to meters
         # Difference between calculated cross center position and actual position (in m)
+        # cross_center still needs to be converted from real grid coordinates to zoomed grid coordinates!!!
         absolute_distance_error = np.linalg.norm([cross_center[0] - cross_center_measured[0], cross_center[1] - cross_center_measured[1]])
         print(f"Absolute distance error = {absolute_distance_error*1e9:.3f} nm")
-        # ===================== Plot =====================
         
+        # ===================== Plot =====================
         if show_plots == True:
             # Plotting the denoised
             plt.figure(figsize=(12,12))

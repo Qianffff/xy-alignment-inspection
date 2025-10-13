e5e0bbf2f92e8d44876ea5d03f777e4caa1690d9
Olivier-Vos <oliviervos@protonmail.com>
2025-10-13 16:36:14 +0200

    -

--- graph.py
+++ graph.py
@@ -8,29 +8,36 @@ def relative_throughput(settings,align_method):
     beam_pitch = settings[3]
     FOV_area = settings[4]
     
+    pixels = int(np.sqrt(FOV_area)/pixel_width)
+    pixel_scan_time = ((N_SE_required / SE_escape_factor) / SE_yield/ collector_efficiency) / (beam_current/e)
+    FOV_scan_time = pixel_scan_time * pixels**2 + beam_overhead_rate*pixels*pixel_width*(pixels-1) # s
+    beam_scan_rate = FOV_area / FOV_scan_time
+    scan_rate = beam_number * beam_scan_rate
+    
     if align_method == "area":
         scanned_area_per_alignment = 24.17*1e-6
     elif align_method == "grid":
         grid_area = (np.sqrt(3/4)*beam_pitch**2) * beam_number # sqrt(3/4) due to hexagonal grid shape
         scanned_area_per_alignment = grid_area
     elif align_method == "time":
-        scanned_area_per_alignment = 24.17e-6
+        accuracy = 18e-9
+        time_error_rate = 0.06e-9 + 0.001e-9*(beam_current*beam_number)/(settings2200[1]*settings2200[2]) # m_error/s
+        stage_movement_error_rate = 1.2278e-6 # m_error/m_stage_movement
+        total_movement_per_grid = (np.sqrt(3/4)*beam_pitch**2)/FOV_area * np.sqrt(FOV_area) + beam_pitch*np.sqrt(beam_number)
+        grid_area = (np.sqrt(3/4)*beam_pitch**2) * beam_number # sqrt(3/4) due to hexagonal grid shape
+        scanned_area_per_alignment = (grid_area / total_movement_per_grid) * (accuracy / stage_movement_error_rate)
+        scanned_time_per_alignment = scanned_area_per_alignment/scan_rate
+        time_error = time_error_rate * scanned_time_per_alignment
+        factor = time_error/accuracy
+        scanned_area_per_alignment *= 1 - factor
     local_alignment_time = get_time(settings,'local')[0]
     
-    pixels = int(np.sqrt(FOV_area)/pixel_width)
-    
-    if beam_number==2791.0:
-        if beam_current == 5e-10:
-            print(local_alignment_time)
-            
-    pixel_scan_time = ((N_SE_required / SE_escape_factor) / SE_yield/ collector_efficiency) / (beam_current/e)
-    FOV_scan_time = pixel_scan_time * pixels**2 + beam_overhead_rate*pixels*pixel_width*(pixels-1) # s
-    beam_scan_rate = FOV_area / FOV_scan_time
-    scan_rate = beam_number * beam_scan_rate
     scan_time_per_alignment = scanned_area_per_alignment / scan_rate
     scanning_fraction = scan_time_per_alignment / (local_alignment_time + scan_time_per_alignment)
     relative_throughput_loss = 1 - scanning_fraction
     return scan_rate, relative_throughput_loss
+
+distribution = .4 # 1 is full on number
 
 def i(method,align_method,n=1000):
     import copy
@@ -44,7 +51,6 @@ def i(method,align_method,n=1000):
         beam_number = np.ones(n)*settings[1]
         beam_current = np.geomspace(0.003e-9,1e-3,n)
     elif method == "split":
-        distribution = .75 # 1 is full on number
         factor = np.geomspace(0.8e-1,400,n)**2
         beam_number = settings[1]*factor**distribution
         beam_current = settings[2]*factor**(1-distribution)
@@ -61,13 +67,24 @@ for align_method in ["area","grid","time"]:
     plt.figure()
     for method in ["number","current","split"]:
         x_arr, y_arr = i(method,align_method)
-        plt.plot(x_arr, y_arr,label=method)
-    plt.plot([88.128,1251.273],[0.018,1.015],'k.',markersize=5)
-    plt.axvline(17978.985,-0.5,15,color='grey', linestyle='--',label='expected throughput eScan 3000')
-    plt.title(align_method)
-    plt.legend()
-    plt.xlim(1200,25000)
-    plt.ylim(-0.5,15)
+        if method == 'number': label = '100/0'
+        elif method == 'current': label = '0/100'
+        elif method == 'split': label = str(int(distribution*100)) + '/' + str(int((1-distribution)*100))
+        plt.plot(x_arr, y_arr,label=label)
+    if align_method == 'time': plt.plot([88.128,1251.48],[0.018,1.27982],'k.',markersize=10)
+    else: plt.plot([88.128,1251.273],[0.018,1.015],'k.',markersize=10)
+    plt.axvline(17978.985,-0.5,15,color='grey', linestyle='--')
+    
+    if align_method == 'area': plt.title('Realign every 24 mm²')
+    elif align_method == 'grid': plt.title('Realign once per grid')
+    elif align_method == 'time': plt.title('Realign based on error build-up')
+    
+    plt.text(1250,2,'eScan 2200',fontsize=10)
+    plt.text(16000,13.5,'eScan 3000',fontsize=10,rotation=90)
+    legend = plt.legend(title='Througput increase due to \n [beam number/beam current] (%):',ncol=3)
+    legend.get_title().set_ha('center')
+    plt.xlim(1100,25000)
+    plt.ylim(0,18)
     plt.xscale("log")
     plt.show(block= align_method=='time')
 
